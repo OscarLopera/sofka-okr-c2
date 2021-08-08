703f1df7d482b5324debde997d9857fd99686051
johngil87 <johngil1487@gmail.com>
2021-08-07 20:11:56 -0500

    se crean los repositorios de conexion a base de datos para usuarios y OKRs, y los entry points para exponer servicios

---
+++ BackSpring/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/okr/c2/mongo/OKRsRepositoryAdapter.java
@@ -0,0 +1,15 @@
+package co.com.sofka.okr.c2.mongo;
+
+import co.com.sofka.okr.c2.mongo.helper.AdapterOperations;
+import org.reactivecommons.utils.ObjectMapper;
+
+import java.util.function.Function;
+
+public class OKRsRepositoryAdapter extends AdapterOperations<Object/* change for domain model */, Object/* change for adapter model */, String, OKRsDBRepository>
+// implements ModelRepository from domain
+{
+
+    public OKRsRepositoryAdapter(OKRsDBRepository repository, ObjectMapper mapper) {
+        super(repository, mapper, d -> mapper.map(d, Object.class/* change for domain model */));
+    }
+}
