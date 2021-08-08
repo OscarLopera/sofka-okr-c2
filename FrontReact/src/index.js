6c1d87f43ec34a2ef56982a25332df44d546a5d2
Diego Urrego <diegourrg@gmail.com>
2021-08-07 20:59:25 -0500

    Arreglé el entry point de la aplicación y se empezó con la creación de los componentes Navbar y Sidebar. Aún en contrucción.

---
+++ FrontReact/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './views/assets/styles/administration/index.css';
+import App from './views/routes/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ FrontReact/src/views/routes/App.jsx
@@ -0,0 +1,11 @@
+import "../assets/styles/administration/index.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Buenasss</h1>
+    </div>
+  );
+}
+
+export default App;
