22637a094e2916fdfd8f7d5a3d6acfcc497de1d8
Nixon <nrgamboa97@gmail.com>
2021-08-08 22:59:40 -0500

    Modelo DDD Okr con value objects y repositorio completo

--- BackNode/domain/okr/okr/Okr.js
+++ BackNode/domain/okr/okr/Okr.js
@@ -1,1 +1,25 @@
+const DescriptionValueObject = require("./values/description");
+const IdValueObject = require("./values/IdValue");
+const ObjectiveValueObject = require("./values/objective");
+const ProgressValueObject = require("./values/progress");
+const TitleValueObject = require("./values/title");
 
+class Okr {
+  constructor(
+    objective,
+    title,
+    managerId,
+    description,
+    verticalId,
+    currentProgress
+  ) {
+    this.objective = ObjectiveValueObject(objective).value;
+    this.title = TitleValueObject(title).value;
+    this.managerId = IdValueObject(managerId).value;
+    this.description = DescriptionValueObject(description).value;
+    this.verticalId = IdValueObject(verticalId).value;
+    this.currentProgress = ProgressValueObject(currentProgress).value;
+  }
+}
+
+module.exports = Okr;
