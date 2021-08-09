77faa675aae0deee9f77dbbbaf633a4b1eafe766
Nixon <nrgamboa97@gmail.com>
2021-08-09 07:59:48 -0500

    Configuracion del servidor

---
+++ BackNode/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+// const routesApi = require("./routes/api-routes");
+//const { logErrors, errorHandler } = require("./utils/middleware/error-handler");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// routesApi(app);
+// app.use(logErrors);
+// app.use(errorHandler);
+
+module.exports = app;
