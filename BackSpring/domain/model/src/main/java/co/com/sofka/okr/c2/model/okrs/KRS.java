6e39917bc19881effaa4b88a86ae88cb080396d0
Cristhian <yurday93@hotmail.com>
2021-08-09 18:55:13 -0500

    Creación del OV HistoricalProgress, la entidad KRS y adición de atributos al agregado OKRS

---
+++ BackSpring/domain/model/src/main/java/co/com/sofka/okr/c2/model/okrs/KRS.java
@@ -0,0 +1,4 @@
+package co.com.sofka.okr.c2.model.okrs;
+
+public class KRS {
+}
---
+++ BackSpring/domain/model/src/main/java/co/com/sofka/okr/c2/model/okrs/values/HistoricalProgress.java
@@ -0,0 +1,20 @@
+package co.com.sofka.okr.c2.model.okrs.values;
+
+public class HistoricalProgress {
+
+    private final String date;
+    private final Double progress;
+
+    public HistoricalProgress(String date, Double progress) {
+        this.date = date;
+        this.progress = progress;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Double getProgress() {
+        return progress;
+    }
+}
