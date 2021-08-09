6f92bbec955c316d221a0d9e5bd5d43633500e28
Nixon <nrgamboa97@gmail.com>
2021-08-09 08:06:21 -0500

    Middleware para manejo de errores

--- BackNode/index.js
+++ BackNode/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 // const routesApi = require("./routes/api-routes");
-//const { logErrors, errorHandler } = require("./utils/middleware/error-handler");
+const { logErrors, errorHandler } = require("./infrastructure/utils/middleware/error-handler");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // routesApi(app);
-// app.use(logErrors);
-// app.use(errorHandler);
+app.use(logErrors);
+app.use(errorHandler);
 
 module.exports = app;
