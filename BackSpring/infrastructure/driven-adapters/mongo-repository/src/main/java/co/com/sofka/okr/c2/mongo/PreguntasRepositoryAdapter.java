0f91db4777ab0422e0b76e2bbe760e5aefa5dd1a
juan carlos salcedo <juank9225@gmail.com>
2021-08-10 16:36:25 -0500

    Se crea el metodo de extraer todas las preguntas frecuentes en preguntasDBRepository

--- BackSpring/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/okr/c2/mongo/PreguntasRepositoryAdapter.java
+++ BackSpring/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/okr/c2/mongo/PreguntasRepositoryAdapter.java
@@ -1,4 +1,27 @@
 package co.com.sofka.okr.c2.mongo;
 
-public class PreguntasRepositoryAdapter {
+import co.com.sofka.okr.c2.model.preguntas.Preguntas;
+import co.com.sofka.okr.c2.model.preguntas.gateways.PreguntasRepository;
+import co.com.sofka.okr.c2.mongo.entities.PreguntasEntity;
+import co.com.sofka.okr.c2.mongo.helper.AdapterOperations;
+import co.com.sofka.okr.c2.mongo.helper.PreguntasMapper;
+import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public class PreguntasRepositoryAdapter extends AdapterOperations<PreguntasEntity, PreguntasEntity, String, PreguntasDBRepository>
+implements PreguntasRepository {
+
+    private PreguntasMapper preguntasMapper = new PreguntasMapper();
+
+    public PreguntasRepositoryAdapter(PreguntasDBRepository repository, ObjectMapper mapper) {
+
+        super(repository, mapper, d -> mapper.map(d, PreguntasEntity.class));
+    }
+
+    @Override
+    public Flux<Preguntas> listPreguntas() {
+        return this.repository.findAll().map(preguntasMapper.fromPreguntasEntity());
+    }
 }
