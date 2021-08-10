426af338b27ae8c12e9f6277db693379ca0a3694
JesusL <thedarkness2555@gmail.com>
2021-08-10 08:29:26 -0500

    Se crea el middleware para OKR

---
+++ FrontReact/src/application/middleware/okr/okr.js
@@ -0,0 +1,76 @@
+import {LOAD_OKRS, ADD_OKRS, UPDATE_OKRS, DELETE_OKRS} from '../../types/okr/okr';
+import {
+    loadOkrsFailure,
+    loadOkrsSuccess,
+    addOkrsSuccess,
+    addOkrsFailure,
+    updateOkrsSuccess,
+    updateOkrsFailure,
+    deleteOkrsSuccess,
+    deleteOkrsFailure, loadOkrs
+} from '../../actions/okr/okr';
+
+const loadOkrFlow = ({api}) => ({dispatch}) => next => async (action) => {
+    next(action);
+    if (action.type === LOAD_OKRS) {
+        try {
+            const okrs = await api.okr.loadOkr()
+            dispatch(loadOkrsSuccess(okrs))
+        } catch (error) {
+            dispatch(loadOkrsFailure(error))
+        }
+    }
+
+}
+
+const addOkrFlow = ({api}) => ({dispatch}) => next => async (action) => {
+    next(action);
+
+    if (action.type === ADD_OKRS) {
+        try {
+            const okrs = await api.okr.createOkr(action.payload)
+            dispatch(addOkrsSuccess(okrs))
+            dispatch(loadOkrs())
+        } catch (error) {
+            dispatch(addOkrsFailure(error))
+        }
+    }
+
+}
+
+const updateOkrFlow = ({api}) => ({dispatch}) => next => async (action) => {
+    next(action);
+    if (action.type === UPDATE_OKRS) {
+        try {
+            const okrs = await api.okr.updateOkr(action.payload)
+            dispatch(updateOkrsSuccess(okrs))
+        } catch (error) {
+            dispatch(updateOkrsFailure(error))
+        }
+    }
+
+}
+
+const deleteOkrFlow = ({api}) => ({dispatch}) => next => async (action) => {
+    next(action);
+    if (action.type === DELETE_OKRS) {
+        try {
+            const okrs = await api.okr.deleteOkr(action.payload)
+            dispatch(deleteOkrsSuccess(okrs))
+            dispatch(loadOkrs())
+        } catch (error) {
+            dispatch(deleteOkrsFailure(error))
+        }
+    }
+
+}
+const middlewareOkr = [
+    loadOkrFlow,
+    addOkrFlow,
+    updateOkrFlow,
+    deleteOkrFlow
+]
+
+export default middlewareOkr
+
+
