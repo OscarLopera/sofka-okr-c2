c5e9ba5abbfe65d9bfdb46c7a77a2048f8f7eac1
nikomaniax <nikomaniax21@gmail.com>
2021-08-09 21:40:53 -0500

    Se Generaliza clase sofka-color-btn para usar en cualquier boton

--- FrontReact/src/views/pages/okr/OkrPage.jsx
+++ FrontReact/src/views/pages/okr/OkrPage.jsx
@@ -6,10 +6,7 @@ const OkrPage = () => {
   return (
     <div>
       <div className="container d-flex justify-content-center py-5">
-        <Button
-          type="button"
-          className="btn create-btn fw-bold py-3 px-4 fs-4 text-uppercase"
-        >
+        <Button type="button" className="btn sofka-color-btn py-3 px-4 fs-4">
           crear okr
         </Button>
       </div>
