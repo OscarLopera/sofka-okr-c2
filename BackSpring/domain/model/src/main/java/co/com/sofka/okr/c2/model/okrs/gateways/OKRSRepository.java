3e723c2bc69a49712872af538da40ea0205722f5
FrankOrtegon <frank.ortegon01@gmail.com>
2021-08-10 11:20:05 -0500

    Se crea el metodo de traer todos los okr por usuario

--- BackSpring/domain/model/src/main/java/co/com/sofka/okr/c2/model/okrs/gateways/OKRSRepository.java
+++ BackSpring/domain/model/src/main/java/co/com/sofka/okr/c2/model/okrs/gateways/OKRSRepository.java
@@ -1,4 +1,9 @@
 package co.com.sofka.okr.c2.model.okrs.gateways;
 
+import co.com.sofka.okr.c2.model.okrs.OKRS;
+import reactor.core.publisher.Flux;
+
 public interface OKRSRepository {
+
+    Flux<OKRS> getAllOkrByUser();
 }
