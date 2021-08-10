8cf2ee30797aeab66704a9049706f0366b9997d8
daniel <juandortiz.15@gmail.com>
2021-08-10 12:37:41 -0500

    se crea use case para listar preguntas

---
+++ BackSpring/domain/usecase/src/main/java/co/com/sofka/okr/c2/usecase/preguntas/ListPreguntasUseCase.java
@@ -0,0 +1,18 @@
+package co.com.sofka.okr.c2.usecase.preguntas;
+
+import co.com.sofka.okr.c2.model.preguntas.Preguntas;
+import co.com.sofka.okr.c2.model.preguntas.gateways.PreguntasRepository;
+import co.com.sofka.okr.c2.model.vertical.Vertical;
+import co.com.sofka.okr.c2.model.vertical.gateways.VerticalRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+
+@RequiredArgsConstructor
+public class ListPreguntasUseCase {
+
+    private final PreguntasRepository preguntasRepository;
+
+    public Flux<Preguntas> execute() {
+        return preguntasRepository.listPreguntas();
+    }
+}
