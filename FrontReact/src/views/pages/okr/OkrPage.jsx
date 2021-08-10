a03dc50c949557d1bd05737e488a0bed5f2427fa
nikomaniax <nikomaniax21@gmail.com>
2021-08-09 23:09:20 -0500

    Mensaje que te invita a crear un OKR cuando no hay

--- FrontReact/src/views/pages/okr/OkrPage.jsx
+++ FrontReact/src/views/pages/okr/OkrPage.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import "../../assets/styles/okr/OkrPage.css";
 import { Button } from "reactstrap";
+import EmptyMessage from "../../components/okr/EmptyMessage";
 
 const OkrPage = () => {
+  const okrs = [];
   return (
-    <div>
-      <div className="container d-flex justify-content-center py-5">
-        <Button type="button" className="btn sofka-color-btn py-3 px-4 fs-4">
-          crear okr
-        </Button>
-      </div>
-      <h1>Okr Page</h1>
+    <div className="container d-flex flex-column align-items-center py-5">
+      {okrs.length === 0 ? (
+        <>
+          <EmptyMessage />
+          <Button type="button" className="btn sofka-color-btn py-3 px-4 fs-4">
+            crear okr
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button type="button" className="btn sofka-color-btn py-3 px-4 fs-4">
+            crear okr
+          </Button>
+          <p>Lista de OKR</p>
+        </>
+      )}
     </div>
   );
 };
