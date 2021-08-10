ca2e0512a52062920b4231b0772d350e4daeba20
daniel <juandortiz.15@gmail.com>
2021-08-10 02:31:47 -0500

    se crea servicio para buscar vertical por id

---
+++ BackSpring/domain/usecase/src/main/java/co/com/sofka/okr/c2/usecase/vertical/ListVerticalUseCase.java
@@ -0,0 +1,16 @@
+package co.com.sofka.okr.c2.usecase.vertical;
+
+import co.com.sofka.okr.c2.model.vertical.Vertical;
+import co.com.sofka.okr.c2.model.vertical.gateways.VerticalRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class ListVerticalUseCase {
+    private final VerticalRepository verticalRepository;
+
+    public Mono<Vertical> listVertical(String id){
+        return  verticalRepository.getVerticalById(id);
+    }
+}
