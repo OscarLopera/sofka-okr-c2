028e8458e2e740163bbcd2ba93f98c3a4e0ba85e
Nixon <nrgamboa97@gmail.com>
2021-08-10 00:57:18 -0500

    Pruebas unitarias, cobertura 100% de aplicacion y dominio

---
+++ BackNode/domain/okr/okr/valueObjects.js
@@ -0,0 +1,13 @@
+const DescriptionValueObject = require("./values/description");
+const IdValueObject = require("./values/IdValue");
+const ObjectiveValueObject = require("./values/objective");
+const ProgressValueObject = require("./values/progress");
+const TitleValueObject = require("./values/title");
+
+module.exports = {
+  DescriptionValueObject,
+  IdValueObject,
+  ObjectiveValueObject,
+  ProgressValueObject,
+  TitleValueObject,
+};
