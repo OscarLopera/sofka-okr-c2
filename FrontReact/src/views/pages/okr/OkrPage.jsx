9e0e283a29236c3b2bea85b5438058eb6dd47246
nikomaniax <nikomaniax21@gmail.com>
2021-08-09 19:00:14 -0500

    Boton para crear OKRs

--- FrontReact/src/views/pages/okr/OkrPage.jsx
+++ FrontReact/src/views/pages/okr/OkrPage.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import "../../assets/styles/okr/OkrPage.css";
+import { Button } from "reactstrap";
 
 const OkrPage = () => {
   return (
     <div>
+      <div className="container d-flex justify-content-center py-5">
+        <Button
+          type="button"
+          className="btn create-btn fw-bold py-3 px-4 fs-4 text-uppercase"
+        >
+          crear okr
+        </Button>
+      </div>
       <h1>Okr Page</h1>
     </div>
   );
