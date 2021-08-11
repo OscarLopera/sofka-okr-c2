f359b7105bf73c49cf03a7dfa13aaaf01e07a982
Nixon <nrgamboa97@gmail.com>
2021-08-11 09:36:36 -0500

    Validacion al actualizar el kr para que este exista

--- BackNode/infrastructure/repositories/okr/krMongoRepository.js
+++ BackNode/infrastructure/repositories/okr/krMongoRepository.js
@@ -29,6 +29,10 @@ class KrRepositoryMongo extends KrRepository {
   }
 
   async updateKr(idKr, krVal) {
+    const kr = await KrSchema.findOne({_id:idKr})
+    if(!kr){
+      throw new Error("Id de Kr a actualizar no existe");
+    }
     const updatedKr = await KrSchema.findByIdAndUpdate({_id:idKr},krVal,{new: true})
     return updatedKr;
   }
