a6aef42a6ac8fafa5b17c8023821d490cf3355e2
SebastianC <sebas99cano@gmail.com>
2021-08-11 15:39:43 -0500

    se realizo la prueba a la conexion de la base de datos

--- BackNode/infrastructure/database/connection.js
+++ BackNode/infrastructure/database/connection.js
@@ -9,19 +9,21 @@ const DB_NAME = config.dbName;
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`;
 
 
-  const connectDB =async ()=>{
-    try{
-        await mongoose
+function callback(err) {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log('Mongodb connected');
+    }
+}
+
+
+const connectDB =async ()=>{
+    await mongoose
         .connect(MONGO_URI,{ useNewUrlParser: true,
-                    useUnifiedTopology: true ,
-                    useCreateIndex: true })
-        console.log("Mongodb connected")
-    }
-    catch(err){
-        console.error(err.message)
-        process.exit(1)
-    }
+            useUnifiedTopology: true ,
+            useCreateIndex: true },callback)
 
 }
 
-module.exports= connectDB
+module.exports= {connectDB,callback}
