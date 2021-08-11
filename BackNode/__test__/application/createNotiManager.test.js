45600951c6dfb57faf48232a4639f94925270b87
Sergio Pineda <sergio.pinedas94@gmail.com>
2021-08-11 00:49:05 -0500

    Se crea test para la creacion de un notification manager

---
+++ BackNode/__test__/application/createNotiManager.test.js
@@ -0,0 +1,30 @@
+const repositoryNotificationManager = require('../../domain/notifications/notificationManager/repositoryNotificationManager');
+const mockRepositoryNotificationManager = new repositoryNotificationManager();
+const createNotiManager = require('../../application/notifications/useCases/createNotiManager');
+const createNewNotificationManager = require('../../application/notifications/useCases/updateNotificationManager');
+const NotificationManager = require('../../domain/notifications/notificationManager/NotificationManager');
+
+test('Happy path test add new notification manager', async () => {
+  // given
+  const persistedNotiManager = new NotificationManager("abc123", "mail", "screen");
+  mockRepositoryNotificationManager.createNotificationManager = jest.fn(() => persistedNotiManager);
+
+  // when
+  const notiManager = await createNotiManager(mockRepositoryNotificationManager, {userId: "abc123", mail:"mail", screen:"screen"} );
+
+  // then
+  expect(mockRepositoryNotificationManager.createNotificationManager).toHaveBeenCalledWith(new NotificationManager ("abc123", "mail", "screen"));
+  expect(notiManager).toEqual(persistedNotiManager);
+});
+test('Sad path test add new notification manager', async () => {
+  // given
+  const persistedNotiManager = new NotificationManager("abc123", "mail", "screen");
+  mockRepositoryNotificationManager.createNotificationManager = jest.fn(() => persistedNotiManager);
+
+  // when
+  const notiManager = await createNewNotificationManager(mockRepositoryNotificationManager, {userId: "abc123", mail:"mail", screen:"screen"} );
+
+  // then
+  expect(mockRepositoryNotificationManager.createNotificationManager).toHaveBeenCalledWith(new NotificationManager ("abc123", "mail", "screen"));
+  expect(notiManager).toEqual(persistedNotiManager);
+});
