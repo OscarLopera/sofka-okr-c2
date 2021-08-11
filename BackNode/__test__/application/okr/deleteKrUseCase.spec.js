7f0d8e33d602a55644cf270af73a51ea9ac0dabb
Santiago Mejia <santiagomejiad19@hotmail.com>
2021-08-11 00:01:55 -0500

    Pruebas para el Delete Kr Caso de uso

---
+++ BackNode/__test__/application/okr/deleteKrUseCase.spec.js
@@ -0,0 +1,22 @@
+const KrRepository = require("../../../domain/okr/kr/KrRepository");
+const krFakes = require("./fakes/krFakes");
+const krServices = require("../../../application/okr/useCases/index");
+const repository = new KrRepository();
+
+describe("delete-kr test", () => {
+  beforeAll(() => {
+    jest
+      .spyOn(repository, "deleteKr")
+      .mockImplementation(() => krFakes.krId);
+  })
+  it("Happypath delete kr succesfully", async () => {
+    const newIdKr = {value:"asfdsfsdfdf"}
+
+    //act
+    const deletedKrId = await krServices.deleteKrUseCase(newIdKr, repository);
+    //assert}
+    expect(repository.deleteKr).toHaveBeenCalled();
+
+    expect(deletedKrId).toBe(krFakes.krId);
+  })
+})
