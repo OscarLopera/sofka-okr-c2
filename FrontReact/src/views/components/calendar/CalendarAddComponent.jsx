1eb7dea7ff35f78b0b300a4d00d375d2153860c4
SebastianC <sebas99cano@gmail.com>
2021-08-11 21:32:48 -0500

    pruebas a componente calendarAddComponent terminada

--- FrontReact/src/views/components/calendar/CalendarAddComponent.jsx
+++ FrontReact/src/views/components/calendar/CalendarAddComponent.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Select from "react-select";
 
 const CalendarAddComponent = ({AddEvent, token}) => {
+
     const currentDate = new Date()
     const date = (currentDate.toISOString().split('T', 8))
     const time= currentDate.getHours()+":"+ currentDate.getMinutes()
@@ -41,7 +42,7 @@ const CalendarAddComponent = ({AddEvent, token}) => {
     }
 
     const clearData = () => {
-      setStartDate(date[0])
+        setStartDate(date[0])
         setDescription("")
         setAttendees([])
         setStartTime(time)
@@ -82,45 +83,72 @@ const CalendarAddComponent = ({AddEvent, token}) => {
 
     return (
         <div>
-            <button className=" mr-3 btn btn-primary px-4" data-testid={"btn-test"} data-toggle={"modal"}
-                    data-target={"#modalAddEvent"}
-            >Agregar Evento <i className="bi bi-plus-square"/>
+            <button className=" mr-3 btn btn-primary px-4"
+                    data-testid={"btn-test-openModalAddEvent"}
+                    data-toggle={"modal"}
+                    data-target={"#modalAddEvent"}>Agregar Evento <i className="bi bi-plus-square"/>
             </button>
             <div id={"modalAddEvent"} className={"modal fade container"}>
                 <div className="modal-dialog modal-lg" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title" id="exampleModalLabel">Agregar Evento</h5>
-                            <button type="button" className={"btn close"} data-dismiss="modal">
-                                <i className="bi bi-x-lg"/>
+                            <button type="button"
+                                    className={"btn close"}
+                                    data-dismiss="modal">
+                                    <i className="bi bi-x-lg"/>
                             </button>
                         </div>
                         <div className="modal-body container row">
                             <label>Dia del Evento</label>
-                            <input type={"date"} min={startDate} value={startDate} className={"form-control"}
+                            <input data-testid={"input-test-date"}
+                                   type={"date"}
+                                   min={startDate}
+                                   value={startDate}
+                                   className={"form-control"}
                                    onChange={event => setStartDate(event.target.value)}/>
                             <label className="col">Hora Inicial</label>
                             <label className="col">Hora Final</label>
                             <div className="w-100"/>
-                            <input placeholder="Selected time" type={"time"} id={"input_starttime"} className={"form-control col"}
-                             onChange={event => setStartTime(event.target.value)}/>
-                            <input placeholder="Selected time" type={"time"} id={"input_endttime"} className={"form-control timepicker col"}
+                            <input data-testid={"input-test-timeStart"}
+                                   placeholder="Selected time" type={"time"}
+                                   id={"input_starttime"}
+                                   className={"form-control col"}
+                                   onChange={event => setStartTime(event.target.value)}/>
+                            <input data-testid={"input-test-timeEnd"}
+                                   placeholder="Selected time"
+                                   type={"time"}
+                                   id={"input_endttime"}
+                                   className={"form-control timepicker col"}
                                    min={startTime}
-                                    onChange={event => setEndTime(event.target.value)}/>
+                                   onChange={event => setEndTime(event.target.value)}/>
                             <hr className="my-4"/>
                             <label>Descripcion</label>
-                            <input type={"text"} value={description} className={"form-control"}
+                            <input data-testid={"input-test-descriptionAddEvent"}
+                                   type={"text"}
+                                   value={description}
+                                   className={"form-control"}
                                    onChange={event => setDescription(event.target.value)}/>
                             <hr className="my-4"/>
                             <label>Invitados</label>
-                            <Select isMulti options={attendeesList} onChange={addAttendees} placeholder={"Selecciona los correos"}/>
+                            <Select isMulti
+                                    options={attendeesList}
+                                    onChange={addAttendees}
+                                    placeholder={"Selecciona los correos"}/>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal"
-                            onClick={() => clearData()}>Cancelar</button>
-                            <button type="button" className="btn btn-primary" data-dismiss="modal"
-                            onClick={() => addEvent()}
-                            >Agregar Evento</button>
+                            <button data-testid={"btn-test-cancelEvent"}
+                                    type="button"
+                                    className="btn btn-secondary"
+                                    data-dismiss="modal"
+                                    onClick={() => clearData()}>
+                                    Cancelar</button>
+                            <button data-testid={"btn-test-addEvent"}
+                                    type="button"
+                                    className="btn btn-primary"
+                                    data-dismiss="modal"
+                                    onClick={() => addEvent()}
+                                    >Agregar Evento</button>
                         </div>
                     </div>
                 </div>
