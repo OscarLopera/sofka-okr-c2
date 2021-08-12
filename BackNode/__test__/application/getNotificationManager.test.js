ee83035add0f72a83b9f46080a3b95158bec0c39
Sergio Pineda <sergio.pinedas94@gmail.com>
2021-08-12 15:59:38 -0500

    Se crea sad path para el caso de uso getNotificationManager

--- BackNode/__test__/application/getNotificationManager.test.js
+++ BackNode/__test__/application/getNotificationManager.test.js
@@ -2,6 +2,7 @@ const repositoryNotificationManager = require('../../domain/notifications/notifi
 const mockRepositoryNotificationManager = new repositoryNotificationManager();
 const getNotificationManager = require('../../application/notifications/useCases/getNotificationManager');
 const NotificationManager = require('../../domain/notifications/notificationManager/NotificationManager');
+const getNoti = require('../../application/notifications/useCases/createNotiManager');
 
 test('Happy path test get notification manager by id', async () => {
   // given
@@ -14,4 +15,18 @@ test('Happy path test get notification manager by id', async () => {
   // then
   expect(mockRepositoryNotificationManager.findNotificationManagerByUserId).toHaveBeenCalledWith("abc123");
   expect(notiManager).toEqual(persistedNotiManager);
+});
+
+test('Sad path test get notification manager by id', async () => {
+  // given
+  const persistedNotiManager = new NotificationManager("abc123", "mail", "screen");
+  mockRepositoryNotificationManager.updateNotificationManager = jest.fn(() => persistedNotiManager);
+
+  // when
+  await getNotificationManager(mockRepositoryNotificationManager, "abc123")
+    .then(() => expect(new Error("Error metodo no implemenado")))
+    .catch((e) => {
+      expect(e).toEqual(new Error("Error metodo no implemenado"))
+    })
+
 });
