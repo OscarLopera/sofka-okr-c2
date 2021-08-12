c3f2066a02f83e7b2075540ddd55040a20bf73e1
MeridaDaniel <Dacastamerd@gmail.com>
2021-08-12 15:05:06 -0500

    Se implemento el caso de uso para obtener todos los usuarios

--- BackNode/infrastructure/server/controllers/calendar/calendarController.js
+++ BackNode/infrastructure/server/controllers/calendar/calendarController.js
@@ -1,9 +1,9 @@
-const { filterUser, userRecommendationByName } = require("../../../../application/calendar/index")
+const { filterUser, userRecommendationByName, getalluser} = require("../../../../application/calendar/index")
 const UserRepositoryMongo = require("../../../repositories/calendar/UserRepository")
 const OkrRepositoryMongo = require("../../../repositories/calendar/OkrRepository")
 const KrRepositoryMongo = require("../../../repositories/calendar/KrRepository")
 
-const {UserRecommendationDTO, UserFilterDTO} = require("../../DTO/")
+const { UserRecommendationDTO, UserFilterDTO } = require("../../DTO/")
 
 const filterUsersOkr = async (req, res, next) => {
     try {
@@ -11,7 +11,7 @@ const filterUsersOkr = async (req, res, next) => {
 
         const filter = await filterUser(id, OkrRepositoryMongo.prototype, KrRepositoryMongo.prototype, UserRepositoryMongo.prototype)
 
-        const filterDTO= filter.map(user => new UserFilterDTO(user))
+        const filterDTO = filter.map(user => new UserFilterDTO(user))
         return res.status(200).json(filterDTO)
 
     } catch (err) {
@@ -24,17 +24,27 @@ const getUsersByNameRegex = async (req, res, next) => {
     try {
         const { name } = req.params;
 
-        const users = await userRecommendationByName(name,UserRepositoryMongo.prototype)
-        const usersDTO= users.map(user => new UserRecommendationDTO(user))
+        const users = await userRecommendationByName(name, UserRepositoryMongo.prototype)
+        const usersDTO = users.map(user => new UserRecommendationDTO(user))
         return res.status(200).json(usersDTO)
-    }catch(err){
+    } catch (err) {
         next(err)
     }
 
 }
 
+const getallusers = async (req, res, next) => {
+    try {
+        const users = await getalluser(UserRepositoryMongo.prototype)
+        const usersDTO = users.map(user => new UserRecommendationDTO(user))
+        return res.status(200).json(usersDTO)
+    } catch (err) {
+        next(err)
+    }
+}
 
 module.exports = {
+    getallusers,
     filterUsersOkr,
     getUsersByNameRegex
 };
