4c9b351239f3a6bc6209e05b795309e3416225e5
Victor Mora <vdaniel.mora@udea.edu.co>
2021-08-12 10:09:19 -0500

    modificacion de los test para adaptarlo a los cambios de la base de datos y el dominio

--- BackNode/__test__/application/calendar/filterUser.test.js
+++ BackNode/__test__/application/calendar/filterUser.test.js
@@ -29,9 +29,9 @@ describe('Get users by OKR id', () => {
         expect(users[0].email).toEqual("juan@gmail.com")
         expect(users[0].urlPhoto).toEqual("https://lh3.googleusercontent.com/ogw/ADea4I5m38rsb3mMMr0Lnosn5W_3JFp5Rx2ErzzkRTOD=s32-c-mo")
         expect(users[0].phone).toEqual("12345678")
-        expect(users[0].isFirstTime).toEqual(true)
+        expect(users[0].firstTime).toEqual(true)
         expect(users[0].verticalId).toEqual(true)
-        expect(users[0].role).toEqual("prfsd165f1s56dueba")
+        expect(users[0].rol).toEqual("prfsd165f1s56dueba")
     })
 
     test('should bring a void list',async () => {
