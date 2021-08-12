6d97c3a2e9b456b84e5792081f268f674b012790
Alejandro <azeron93@gmail.com>
2021-08-12 10:48:51 -0500

    se añade socket para notificaciones realtime

--- BackNode/infrastructure/server/server.js
+++ BackNode/infrastructure/server/server.js
@@ -6,5 +6,17 @@ const app = require("../../index");
 const server = app.listen(config.port, () => {
   console.log(`listen at http://localhost:${config.port}`);
 });
+const io = socket(server,{
+  cors: {
+    origin: '*',
+  }
+});
+io.on("connection", (socket)=>{
+  console.log("conectado")
+  socket.on("actualizar-notis",(data)=>{
+      io.sockets.emit((data.id),`${data.manager} se han actualizado tus preferencias de notificaciones`)
+  })
+})
+
 
 module.exports = server
--- BackNode/infrastructure/server/socket/socketNoti.js
+++ BackNode/infrastructure/server/socket/socketNoti.js
@@ -1,17 +1,17 @@
-const server = require('../server')
-const socket = require('socket.io')
+// const server = require('../server')
+// const socket = require('socket.io')
 
 
-const io = socket(server,{
-    cors: {
-      origin: '*',
-    }
-  });
-io.on("connection", (socket)=>{
-    console.log("conectado")
-    socket.on("actualizar-notis",(data)=>{
-        io.sockets.emit((data.id),`${data.manager} se han actualizado tus preferencias de notificaciones`)
-    })
-})
+// // const io = socket(server,{
+// //     cors: {
+// //       origin: '*',
+// //     }
+// //   });
+// // io.on("connection", (socket)=>{
+// //     console.log("conectado")
+// //     socket.on("actualizar-notis",(data)=>{
+// //         io.sockets.emit((data.id),`${data.manager} se han actualizado tus preferencias de notificaciones`)
+// //     })
+// // })
 
-module.exports = io
+// module.exports = io
