e6e19d9d2d69f58a9d1cbe9a52940d785fefa990
MeridaDaniel <Dacastamerd@gmail.com>
2021-08-12 12:31:35 -0500

    Se re estructuro y corrigio el modal del componente agregar evento

--- FrontReact/src/views/components/calendar/CalendarAddComponent.jsx
+++ FrontReact/src/views/components/calendar/CalendarAddComponent.jsx
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Select from "react-select";
+import Modal from 'react-bootstrap/Modal'
 
-export const CalendarAddComponent = ({AddEvent, token}) => {
+export const CalendarAddComponent = ({ AddEvent, token }) => {
 
     let currentDate = new Date()
 
     const date = (currentDate.toISOString().split('T', 8))
     const time = currentDate.getHours() + ":" + currentDate.getMinutes()
 
+    const [show, setShow] = useState(false);
     const [startDate, setStartDate] = useState(date[0]);
     const [description, setDescription] = useState("");
     const [attendees, setAttendees] = useState([]);
@@ -16,23 +18,23 @@ export const CalendarAddComponent = ({AddEvent, token}) => {
     const [externalAttendees, setExternalAttendees] = useState("");
     const [attendeesList, setAttendeesList] = useState([
         {
-            value: {email: "sebas99cano@gmail.com"},
+            value: { email: "sebas99cano@gmail.com" },
             label: "sebas99cano@gmail.com"
         },
         {
-            value: {email: "sebas.cano1036@gmail.com"},
+            value: { email: "sebas.cano1036@gmail.com" },
             label: "sebas.cano1036@gmail.com"
         },
         {
-            value: {email: "anahernandez814@gmail.com"},
+            value: { email: "anahernandez814@gmail.com" },
             label: "anahernandez814@gmail.com"
         },
         {
-            value: {email: "danielaristy22@gmail.com"},
+            value: { email: "danielaristy22@gmail.com" },
             label: "danielaristy22@gmail.com"
         },
         {
-            value: {email: "dacastamerd@gmail.com"},
+            value: { email: "dacastamerd@gmail.com" },
             label: "dacastamerd@gmail.com"
         }
     ])
@@ -44,6 +46,9 @@ export const CalendarAddComponent = ({AddEvent, token}) => {
         })
         setAttendees(Array.isArray(e) ? e.map(x => x.value) : []);
     }
+    const handleClose = () => setShow(false);
+
+    const handleShow = () => setShow(true);
 
     const clearData = () => {
         setStartDate(date[0])
@@ -51,12 +56,14 @@ export const CalendarAddComponent = ({AddEvent, token}) => {
         setStartTime(time)
         setEndTime(time)
         setExternalAttendees("")
+        setShow(false)
     }
 
     const addEvent = () => {
-        if(externalAttendees!==""){
+
+        if (externalAttendees !== "") {
             let inv = attendees;
-            inv.push({email: externalAttendees});
+            inv.push({ email: externalAttendees });
             setAttendees(inv)
         }
         const eventObject = {
@@ -73,7 +80,7 @@ export const CalendarAddComponent = ({AddEvent, token}) => {
             conferenceData: {
                 createRequest: {
                     requestId: "sample13",
-                    conferenceSolutionKey: {type: "hangoutsMeet"}
+                    conferenceSolutionKey: { type: "hangoutsMeet" }
                 },
             },
             attendees: attendees,
@@ -88,96 +95,85 @@ export const CalendarAddComponent = ({AddEvent, token}) => {
         setEndTime("")
         AddEvent(eventObject, token)
         clearData()
+        handleClose()
     }
 
     return (
         <div>
             <button className=" mr-3 btn btn-primary px-4"
-                    data-testid={"btn-test-openModalAddEvent"}
-                    data-toggle={"modal"}
-                    data-target={"#modalAddEvent"}>Agregar Evento <i className="bi bi-plus-square"/>
+                onClick={handleShow}>Agregar Event0 <i className="bi bi-plus-square" />
             </button>
-            <div id={"modalAddEvent"} className={"modal fade container"}>
-                <div className="modal-dialog modal-lg" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Agregar Evento</h5>
-                            <button type="button"
-                                    className={"btn close"}
-                                    data-dismiss="modal">
-                                <i className="bi bi-x-lg"/>
-                            </button>
-                        </div>
-                        <div className="modal-body container row">
-                            <form onSubmit={addEvent} >
-                                <label>Dia del Evento</label>
-                                <input data-testid={"input-test-date"}
-                                       required={true}
-                                       type={"date"}
-                                       min={date[0]}
-                                       value={startDate}
-                                       className={"form-control"}
-                                       onChange={event => setStartDate(event.target.value)}/>
-                                <hr className="my-4"/>
-                                <label className="col">Hora Inicial</label>
-                                <input data-testid={"input-test-timeStart"}
-                                       required={true}
-                                       placeholder="Selected time" type={"time"}
-                                       id={"input_starttime"}
-                                       className={"form-control col"}
-                                       onChange={event => setStartTime(event.target.value)}/>
-                                <hr className="my-4"/>
-                                <label className="col">Hora Final</label>
-                                <input data-testid={"input-test-timeEnd"}
-                                       required={true}
-                                       placeholder="Selected time"
-                                       type={"time"}
-                                       id={"input_endttime"}
-                                       className={"form-control timepicker col"}
-                                       min={startTime}
-                                       onChange={event => setEndTime(event.target.value)}/>
-                                <hr className="my-4"/>
-                                <label>Descripcion</label>
-                                <input data-testid={"input-test-descriptionAddEvent"}
-                                       required={true}
-                                       minLength={5}
-                                       maxLength={20}
-                                       type={"text"}
-                                       value={description}
-                                       className={"form-control"}
-                                       onChange={event => setDescription(event.target.value)}/>
-                                <hr className="my-4"/>
-                                <label>Invitados Internos</label>
-                                <Select isMulti
-                                        options={attendeesList}
-                                        onChange={addAttendees}
-                                        placeholder={"Selecciona los correos"}/>
-                                <hr className="my-4"/>
-                                <label>Invitados Externos</label>
-                                <input data-testid={"input-test-external"}
-                                       className={"form-control"}
-                                       minLength={5}
-                                       maxLength={35}
-                                       type={"email"}
-                                       value={externalAttendees}
-                                       onChange={event => setExternalAttendees(event.target.value)}/>
-                                <hr className="my-4"/>
-                                <button data-testid={"btn-test-cancelEvent"}
-                                        type="button"
-                                        className={"btn btn-secondary px-5 mr-4"}
-                                        data-dismiss="modal"
-                                        onClick={() => clearData()}>
-                                    Cancelar
-                                </button>
-                                <button data-testid={"btn-test-addEvent"}
-                                        type="submit"
-                                        className="btn btn-primary px-5 ">Agregar Evento
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
+            <Modal size="lg" show={show} onHide={handleClose} aria-labelledby="example-modal-sizes-title-lg">
+                <Modal.Header>
+                    <Modal.Title id="example-modal-sizes-title-lg">
+                        <h5 className="modal-title" id="exampleModalLabel">Agregar Evento</h5>
+                    </Modal.Title>
+                    <closeButton type="button" onClick={clearData}><i className="bi bi-x-lg" /></closeButton>
+                </Modal.Header>
+                <Modal.Body>
+                    <form onSubmit={addEvent} >
+                        <label>Dia del Evento</label>
+                        <input data-testid={"input-test-date"}
+                            required={true}
+                            type={"date"}
+                            min={date[0]}
+                            value={startDate}
+                            className={"form-control"}
+                            onChange={event => setStartDate(event.target.value)} />
+                        <hr className="my-4" />
+                        <label className="col">Hora Inicial</label>
+                        <input data-testid={"input-test-timeStart"}
+                            required={true}
+                            placeholder="Selected time" type={"time"}
+                            id={"input_starttime"}
+                            className={"form-control col"}
+                            onChange={event => setStartTime(event.target.value)} />
+                        <hr className="my-4" />
+                        <label className="col">Hora Final</label>
+                        <input data-testid={"input-test-timeEnd"}
+                            required={true}
+                            placeholder="Selected time"
+                            type={"time"}
+                            id={"input_endttime"}
+                            className={"form-control timepicker col"}
+                            min={startTime}
+                            onChange={event => setEndTime(event.target.value)} />
+                        <hr className="my-4" />
+                        <label>Descripcion</label>
+                        <input data-testid={"input-test-descriptionAddEvent"}
+                            required={true}
+                            minLength={5}
+                            maxLength={20}
+                            type={"text"}
+                            value={description}
+                            className={"form-control"}
+                            onChange={event => setDescription(event.target.value)} />
+                        <hr className="my-4" />
+                        <label>Invitados Internos</label>
+                        <Select isMulti
+                            options={attendeesList}
+                            onChange={addAttendees}
+                            placeholder={"Selecciona los correos"} />
+                        <hr className="my-4" />
+                        <label>Invitados Externos</label>
+                        <input data-testid={"input-test-external"}
+                            className={"form-control"}
+                            minLength={5}
+                            maxLength={35}
+                            type={"email"}
+                            value={externalAttendees}
+                            onChange={event => setExternalAttendees(event.target.value)} />
+                        <hr className="my-4" />
+                        <button type="button" className={"btn btn-secondary px-5 mr-4"} onClick={() => clearData()}>
+                             Cancelar
+                        </button>
+                        <button type="submit" className="btn btn-primary px-5">
+                             Agregar Evento
+                        </button>
+                    </form>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
