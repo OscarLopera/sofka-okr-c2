c9b215dcf127adc2b007292b85d7a6b3c781458a
Nixon <nrgamboa97@gmail.com>
2021-08-12 19:01:07 -0500

    ValueObjects

--- BackNode/domain/okr/okr/ValueObjects.js
+++ BackNode/domain/okr/okr/ValueObjects.js
@@ -1,9 +1,9 @@
 const DescriptionValueObject = require("./values/Description");
 const IdValueObject = require("./values/IdValue");
+
 const ObjectiveValueObject = require("./values/Objective");
 const ProgressValueObject = require("./values/Progress");
 const TitleValueObject = require("./values/Title");
-
 
 module.exports = {
   DescriptionValueObject,
