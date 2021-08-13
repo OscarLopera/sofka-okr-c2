c5f76f6dfcd10db365cdd87045474cf21aa0a6e7
SebastianC <sebas99cano@gmail.com>
2021-08-12 23:46:50 -0500

    se arreglo el timepo del token, implementando una fucnion en el useefect de calendarpage

--- FrontReact/src/views/pages/calendar/CalendarPage.jsx
+++ FrontReact/src/views/pages/calendar/CalendarPage.jsx
@@ -13,12 +13,32 @@ import CalendarAddComponent from "../../components/calendar/CalendarAddComponent
 import {getUser} from "../../../application/selectors/administration/user";
 import TableEventComponent from '../../components/calendar/TableEventComponent'
 import CalendarComponent from '../../components/calendar/CalendarComponent'
+import firebase from "firebase";
 
-export const CalendarPage = ({events, AddEvent, ListEvents, DeleteEvent,UpdateEvent, user, emails, GetEmailUsers}) => {
+
+export const CalendarPage = ({events, AddEvent, ListEvents, DeleteEvent, UpdateEvent, user, emails, GetEmailUsers}) => {
 
     useEffect(() => {
         ListEvents(user.userToken)
         GetEmailUsers()
+        const auth = firebase.auth();
+        auth.onAuthStateChanged((user) => {
+                let sessionTimeout = null;
+                if (user === null) {
+                    // User is logged out.
+                    // Clear the session timeout.
+                    sessionTimeout && clearTimeout(sessionTimeout);
+                    sessionTimeout = null;
+                } else {
+                    user.getIdTokenResult().then((idTokenResult) => {
+                        const authTime = idTokenResult.claims.auth_time * 1000;
+                        const sessionDuration = 1000 * 60 * 60 * 24 * 30;
+                        const millisecondsUntilExpiration = sessionDuration - (Date.now() - authTime);
+                        setTimeout(() => auth.signOut(), millisecondsUntilExpiration);
+                    });
+                }
+            }
+        )
     }, [GetEmailUsers, ListEvents, user.userToken])
 
     return (
@@ -26,11 +46,12 @@ export const CalendarPage = ({events, AddEvent, ListEvents, DeleteEvent,UpdateEv
             <div className="row">
                 <div className="col-md-12">
                     <h1>Eventos</h1>
-                    <CalendarAddComponent AddEvent={AddEvent} token={user.userToken} userEmails={emails} />
+                    <CalendarAddComponent AddEvent={AddEvent} token={user.userToken} userEmails={emails}/>
                 </div>
             </div>
             <div className={"row"}>
-                <TableEventComponent events={events} DeleteEvent={DeleteEvent} token={user.userToken} UpdateEvent={UpdateEvent} email={user.userEmail} userEmails={emails}/>
+                <TableEventComponent events={events} DeleteEvent={DeleteEvent} token={user.userToken}
+                                     UpdateEvent={UpdateEvent} email={user.userEmail} userEmails={emails}/>
             </div>
             <br/><br/>
             <CalendarComponent events={events}/>
@@ -47,7 +68,7 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({AddEvent, ListEvents, DeleteEvent,UpdateEvent,GetEmailUsers}, dispatch);
+    return bindActionCreators({AddEvent, ListEvents, DeleteEvent, UpdateEvent, GetEmailUsers}, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CalendarPage);
